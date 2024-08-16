f2c6146b040c8865f717c9acdab396577bd5d823
s6nke <soenkebartels@live.de>
2024-08-16 20:16:26 +0200

    improvement(nvim): added lsp config

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,3 +1,15 @@
+local lsp_zero = require("lsp-zero")
+
+local lsp_attach = function(client,bufnr)
+    local opts = {buffer = bufnr}
+end
+
+lsp_zero.extend_lspconfig({
+    sign_text = true,
+    lsp_attach = lsp_attach,
+    capabilities = require("cmp_nvim_lsp").default_capabilities(),
+})
+
 local cmp = require("cmp")
 
 cmp.setup({
